deb047116a4398d0ff21802c901a004bc85832a2
Canaboss <131531838+ablayefaye04@users.noreply.github.com>
2023-12-16 21:25:03 +0000

    Update models.py

--- website/Forum/models.py
+++ website/Forum/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#creation de la classe MESSAGE
+"""""
+avec les attribut suivants 
+--Saisir message.  (type text )
+--inclure une image (OPTIONNELLE)
+--Envoyer  (fonction)
+--Profile user
+--Date creation du message .
+
+"""
+
